df0bd145278212c6bb0d6d1ac2e8add0cff7d242
jmorgan <jamorgan8@gmail.com>
2023-05-02 13:23:54 -0500

    Added complete feature

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-##obtain user input and save as a variable
-user_prompt = "Type add, show, or exit: "
+## initialize empty list
 todos = []
 
 while True:
-    user_action = input(user_prompt)
+    user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-## match literally attempts to match what is input to the specified variable. Similar to case/when in SQL
+## match function literally attempts to match what is input to the specified variable. Similar to case/when in SQL
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+
         case 'show':
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos): # iterates over todos list and prints index + 1 and item
+                print(f"{index+1}- {item}") #f string prints literal string as defined inside quotes
+
+        case 'edit':
+            number = int(input("Enter number of todo to edit: ")) ## int converts str.
+            number = number - 1 ## to correct for the python index starting at 0
+            new_todo = input("Enter new todo: ") ## stores new item
+            todos[number] = new_todo ## stores new item to todos list at location of original
+
+        case 'complete':
+            number = int(input("Enter number of todo to complete: "))
+            removed = todos.pop(number-1)
+            print('Great work! You finished: ', removed)
+
         case 'exit':
             break ##breaks while loop
 
