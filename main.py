c74af3c3c26925df6efcdd19f5a081d5930d7182
jmorgan <jamorgan8@gmail.com>
2023-05-06 10:20:18 -0500

    added read/writing to text file

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-## initialize empty list
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
 
-## match function literally attempts to match what is input to the specified variable. Similar to case/when in SQL
+# match function literally attempts to match what is input to the specified variable. Similar to case/when in SQL
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n" # "\n" is a line break
+
+            # open function creates a file object. open function takes 2 arguments- the file path and a character
+            # code (r, a, w, x).
+            # This chunk of code opens and saves the contents of the text file as a list then closes the file.
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close() # close file object to make sure other aspects of code don't interact
+
+            # Adds the new entry to list
             todos.append(todo)
 
+            # opens text file again and replaces contents with new list which is the original contents plus the addition
+            file = open('todos.txt', 'w')
+            # method for file objects. Takes 1 argument which must be a list
+            file.writelines(todos)
+            file.close()
+
         case 'show':
-            for index, item in enumerate(todos): # iterates over todos list and prints index + 1 and item
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            for index, item in enumerate(new_todos): # iterates over todos list and prints index + 1 and item
+                item = item.strip('\n')
                 print(f"{index+1}- {item}") #f string prints literal string as defined inside quotes
 
         case 'edit':
             number = int(input("Enter number of todo to edit: ")) ## int converts str.
-            number = number - 1 ## to correct for the python index starting at 0
+            number = number - 1 # to correct for the python index starting at 0
             new_todo = input("Enter new todo: ") ## stores new item
-            todos[number] = new_todo ## stores new item to todos list at location of original
+            todos[number] = new_todo # stores new item to todos list at location of original
 
         case 'complete':
             number = int(input("Enter number of todo to complete: "))
@@ -27,7 +44,7 @@ while True:
             print('Great work! You finished: ', removed)
 
         case 'exit':
-            break ##breaks while loop
+            break # breaks while loop
 
 print("Bye!")
 
